94d1af4127ad778b6ff48a20932ed6bb5e7a6b32
Elena Tolpygo <etolpygo@gmail.com>
2014-05-24 00:45:59 -0700

    first feed implementation

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -83,6 +83,20 @@ describe "Authentication Pages" do
           specify { expect(response).to redirect_to(signin_path) }
         end
       end
+      
+      
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+            
 
       describe "in the Users controller" do
 
@@ -101,6 +115,16 @@ describe "Authentication Pages" do
           before { visit users_path }
           specify { expect(page).to have_title('Login') }
           specify { expect(page).to have_notice_message(/Please sign in/) }
+        end
+        
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          specify { expect(page).to have_title('Login') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          specify { expect(page).to have_title('Login') }
         end
         
       end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -74,6 +74,56 @@ describe "User pages" do
       specify { expect(page).to have_content(m2.content) }
       specify { expect(page).to have_content(user.microposts.count) }
     end
+    
+    describe "follow/unfollow buttons" do
+      let(:other_user) { FactoryGirl.create(:user) }
+      before { sign_in user }
+
+      describe "following a user" do
+        before { visit user_path(other_user) }
+
+        it "should increment the followed user count" do
+          expect do
+            click_button "Follow"
+          end.to change(user.followed_users, :count).by(1)
+        end
+
+        it "should increment the other user's followers count" do
+          expect do
+            click_button "Follow"
+          end.to change(other_user.followers, :count).by(1)
+        end
+
+        describe "toggling the button" do
+          before { click_button "Follow" }
+          specify { expect(page).to have_xpath("//input[@value='Unfollow']") }
+        end
+      end
+
+      describe "unfollowing a user" do
+        before do
+          user.follow!(other_user)
+          visit user_path(other_user)
+        end
+
+        it "should decrement the followed user count" do
+          expect do
+            click_button "Unfollow"
+          end.to change(user.followed_users, :count).by(-1)
+        end
+
+        it "should decrement the other user's followers count" do
+          expect do
+            click_button "Unfollow"
+          end.to change(other_user.followers, :count).by(-1)
+        end
+
+        describe "toggling the button" do
+          before { click_button "Unfollow" }
+          specify { expect(page).to have_xpath("//input[@value='Follow']") }
+        end
+      end
+    end
 
   end # end describe "profile page"
 
@@ -170,5 +220,36 @@ describe "User pages" do
     end # end describe "with valid information"
     
   end # end edit
+  
+######################################
+
+  describe "following/followers" do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:other_user) { FactoryGirl.create(:user) }
+    before { user.follow!(other_user) }
+
+    describe "followed users" do
+      before do
+        sign_in user
+        visit following_user_path(user)
+      end
+
+      specify { expect(page).to have_title(full_title('Following')) }
+      specify { expect(page).to have_selector('h3', text: 'Following') }
+      specify { expect(page).to have_link(other_user.name, href: user_path(other_user)) }
+    end
+
+    describe "followers" do
+      before do
+        sign_in other_user
+        visit followers_user_path(other_user)
+      end
+
+      specify { expect(page).to have_title(full_title('Followers')) }
+      specify { expect(page).to have_selector('h3', text: 'Followers') }
+      specify { expect(page).to have_link(user.name, href: user_path(user)) }
+    end
+    
+  end # end describe "following/followers"
   
 end # end describe "User pages"
